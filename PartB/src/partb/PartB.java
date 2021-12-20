ce2aff4d73e95e42dec5b78ec5a1faf906f24350
WaterDroplet89 <luke_patrick_vasquez@live.com>
2021-12-21 01:10:42 +0800

    1

--- PartB/src/partb/PartB.java
+++ PartB/src/partb/PartB.java
@@ -1,10 +1,19 @@
 package partb;
+import javax.swing.JOptionPane;
 
 public class PartB {
 
    
     public static void main(String[] args) {
+        String name;
+        String a1 = JOptionPane.showInputDialog(null,"Please input your department name","Input",JOptionPane.QUESTION_MESSAGE);
+        name = a1;
+        System.out.println(name);
         
+        
+        String a2 = JOptionPane.showInputDialog(null,"Please input number of staff your department","Input",JOptionPane.QUESTION_MESSAGE);
+        int no = Integer.parseInt(a2);
+        System.out.println(no);
     }
     
 }
