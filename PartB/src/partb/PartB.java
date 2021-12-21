caf48ff949797a443da705a494fdf8a9f62dd6fb
WaterDroplet89 <luke_patrick_vasquez@live.com>
2021-12-21 22:15:17 +0800

    1

--- PartB/src/partb/PartB.java
+++ PartB/src/partb/PartB.java
@@ -8,7 +8,7 @@ public class PartB {
     public static void main(String[] args) {
         String Name, initials, Dep, a1 = "", a2 = "", a3 = "", a, Timer = "";
         char rep = 'n';
-        int ID, NoStaff = 0;
+        int ID=0, NoStaff = 0;
 
         //The First Group Of Code Asks The Name Of The Department From User, It Also Check If The Input Contains Any Numbers And Special Characters, Will Throw Error If Numbers And Special Characters Is Present
         do {
@@ -38,16 +38,24 @@ public class PartB {
         do {
             rep = 'n';
             a1 = JOptionPane.showInputDialog(null, "Please Input Your Full Name, Separated By Space", "Input", JOptionPane.QUESTION_MESSAGE);
-            if (!a1.matches("^[a-zA-Z_ ]*$")) {
+            if (!a1.matches("^[a-zA-Z ]*$")) {
 
                 JOptionPane.showMessageDialog(null, "Please Input Characters Only", "Error", JOptionPane.ERROR_MESSAGE);
                 rep = 'y';
             }
         } while (rep == 'y');
 
-        //This Group Of Code Asks For The User's ID Number, It Also Check If The Inputted ID Number is Larger Than Number Of Staff In User's Department, If ID Is Larger Than Number Of Staff, Throw Error And Repeat
-        String c = JOptionPane.showInputDialog(null, "Please Input Your ID Number", "Input", JOptionPane.QUESTION_MESSAGE);
-        ID = Integer.parseInt(c);
+        //This Group Of Code Asks For The User's ID Number, It Also Check If The Input Contains Any Characters And Special Characters, Will Throw Exception If Characters And Special Characters Is Present
+        do {
+            String c = JOptionPane.showInputDialog(null, "Please Input Your ID Number", "Input", JOptionPane.QUESTION_MESSAGE);
+            try {
+                ID = Integer.parseInt(c);
+                rep = 'n';
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Please Input Numbers Only", "ERROR", JOptionPane.ERROR_MESSAGE);
+                rep = 'y';
+            }
+        } while (rep == 'y');
 
         //This Code Gets The Users Job Designation, Done By Calling A Method From Another Class
         String JobDesig = Job.Desig();
