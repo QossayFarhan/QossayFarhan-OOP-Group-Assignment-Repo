c65c7689d0a43a69c334527f704a31d5750c61ed
theranae <ranaarafa28@gmail.com>
2021-12-22 16:40:18 +0300

    created the necessary answer for part a

---
+++ group_assignment/src/group_assignment/Question2_a.java
@@ -0,0 +1,47 @@
+
+/**
+ * ARRAY COMPARISON
+ * 
+ *   finding the issue on why the output is in such a way.
+ * 
+ * @author (RANA MAHAMOUD SAEED - B09200009)
+ * @version 1.x
+ */
+package group_assignment;
+
+
+public class Question2_a {
+    
+    public static void main(String[] args) {
+        
+        int arr1[ ] = {34, 54, 90, 45, 89, 42, 88, 22, 19, 49};
+        int arr2[ ] = {34, 54, 90, 45, 89, 42, 88, 22, 19, 49};
+ 
+           if (arr1 == arr2) 
+           System.out.println("Same");
+           else
+           System.out.println("Not same");
+ 
+      arr1 = new int[5];
+      System.out.println("arr1[4] is " + arr1[4]);
+      System.out.println("arr2[4] is " + arr2[4]);
+ }
+
+/**
+ * the output displays
+ *                    "Not Same
+ *                     arr1[4] is 0
+ *                     arr2[4] is 89"
+ * 
+ * the output is in such a way because for the first part it is
+ * comparing the two arrays as objects therefore they're not the same 
+ * as they're considered two different objects.
+ * 
+ * for the second part it says that arr1[4] is 0 because we added a new 
+ * integer element to index number 4 in arr1 and since its element was not
+ * declared, it was declared as 0 by default.
+ * 
+ * 
+ */
+    }
+  
