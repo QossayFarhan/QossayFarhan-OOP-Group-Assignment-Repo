50629671bac9cb32ac3ba6c3845b39cf62eb21c9
WaterDroplet89 <luke_patrick_vasquez@live.com>
2021-12-26 15:50:39 +0800

    1

--- PartB/src/MainFrontend/MyFrontend.java
+++ PartB/src/MainFrontend/MyFrontend.java
@@ -1,31 +1,30 @@
-
-package MainFrontend; 
+package MainFrontend;
 
 import Myclasses.*;
 import javax.swing.JOptionPane;
 
 public class MyFrontend {
+
     public static void main(String[] args) {
-        
+
         //Parametrs for control statments  
         boolean repeat;
         String input;
         int weekNo = 1;
         //Temporary variables to recive the values from the user then Pass it to the correct constroctor
-        String department; 
+        String department;
         int noStaff = 0;
         String name;
         int ID = 0;
         String position;
-        boolean Timer; 
-        
-        
+        boolean Timer;
+
         /*The First Group Of Code Asks The Name Of The Department From User
         *It Also Check If The Input Contains Any Numbers And Special Characters, 
         *Will Throw Error If Numbers And Special Characters Is Present
-        */
+         */
         do {
-            repeat = false ;
+            repeat = false;
             department = JOptionPane.showInputDialog(null, "Please Input Your Department Name", "Input", JOptionPane.QUESTION_MESSAGE);
             if (!department.matches("[a-zA-Z ]+")) {
 
@@ -34,11 +33,11 @@ public class MyFrontend {
             }
         } while (repeat
                 == true);
-        
+
         /*The Second Group Of Code Asks The Number Of Staff In The Department From User, 
         *It Also Check If The Input Contains Any Characters And Special Characters,
         *Will Throw Exception If Characters And Special Characters Is Present
-        */
+         */
         do {
             do {
                 repeat = false;
@@ -66,8 +65,7 @@ public class MyFrontend {
         /*This Group Of Code Ask For The User's Full Name, It Also Check 
         *If The Input Contains Any Numbers And Special Characters,
         *Will Throw Error If Numbers And Special Characters Is Present
-        */
-        
+         */
         do {
             repeat = false;
             name = JOptionPane.showInputDialog(null, "Please Input Your Full Name, Separated By Space", "Input", JOptionPane.QUESTION_MESSAGE);
@@ -81,7 +79,7 @@ public class MyFrontend {
         /*This Group Of Code Asks For The User's ID Number,
         *It Also Check If The Input Contains Any Characters And Special Characters,
         *Will Throw Exception If Characters And Special Characters Is Present
-        */
+         */
         do {
             do {
                 repeat = false;
@@ -104,10 +102,10 @@ public class MyFrontend {
                 repeat = true;
             }
         } while (repeat == true);
-        
+
         //call a dropdown menu from InterfaceElements class to chose position 
         position = InterfaceElemnts.Desig();
-        
+
         //This Code Determines Whether The User Is A Full Timer Or Part Timer, Done By Calling A Method From Another Class
         do {
             repeat = false;
@@ -118,7 +116,7 @@ public class MyFrontend {
                 Timer = true;
             } else {
                 Timer = false;
-                JOptionPane.showMessageDialog(null, position + " is not applicable for " + Timer, "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, position + " is not applicable for Part-Timer", "Error", JOptionPane.ERROR_MESSAGE);
                 int choice = JOptionPane.showConfirmDialog(null, "Would You Like To Choose Your Job Designation Again?", "Please Choose", JOptionPane.YES_NO_OPTION);
                 if (choice == 0) {
                     position = Job.Desig();
@@ -126,93 +124,95 @@ public class MyFrontend {
                 repeat = true;
             }
         } while (repeat == true);
-        
-        
+
         //create an object based on the correct class that has been chosen by user(position)
-        Employee employee = null ;
+        Employee employee = null;
         switch (position) {
-            case "Manager":{
-                employee = new Manager(department,noStaff,name,position,Timer);
-                    break;
-                }
-            case "Assistant Manager":{
-                employee = new AssistantManager(department,noStaff,name,position,Timer);
-                    break;
-                }
-            case "Supervisor":{
-                employee = new Supervisor(department,noStaff,name,position,Timer);
-                    break;
-                }
-            case "Executive":{
-                employee = new Executive(department,noStaff,name,position,Timer);
-                    break;
-                }
-            case "Promoters":{
-                employee = new Promoter(department,noStaff,name,position,Timer);
-                    break;
-                }
-            case "Telemarketers":{
-                employee = new Telemarketer(department,noStaff,name,position,Timer);
-                    break;
-                }
+            case "Manager": {
+                employee = new Manager(department, noStaff, name, position, Timer);
+                break;
+            }
+            case "Assistant Manager": {
+                employee = new AssistantManager(department, noStaff, name, position, Timer);
+                break;
+            }
+            case "Supervisor": {
+                employee = new Supervisor(department, noStaff, name, position, Timer);
+                break;
+            }
+            case "Executive": {
+                employee = new Executive(department, noStaff, name, position, Timer);
+                break;
+            }
+            case "Promoters": {
+                employee = new Promoter(department, noStaff, name, position, Timer);
+                break;
+            }
+            case "Telemarketers": {
+                employee = new Telemarketer(department, noStaff, name, position, Timer);
+                break;
+            }
             default:
                 break;
         }
-        
+
         // Set hour Price and allowance
         employee.SetPrice();
-        
-        
+
         //here we call methods from InterfaceElemnts class to input the number of working hours for each week and check its validity 
         employee.week1 = InterfaceElemnts.InputWeek(employee.FullTime, weekNo);
         employee.LessHoursReason1 = InterfaceElemnts.LessHoursReason(weekNo, employee.week1, employee.FullTime);
         weekNo += 1;
-        
+
         employee.week2 = InterfaceElemnts.InputWeek(employee.FullTime, weekNo);
         employee.LessHoursReason2 = InterfaceElemnts.LessHoursReason(weekNo, employee.week2, employee.FullTime);
         weekNo += 1;
-        
+
         employee.week3 = InterfaceElemnts.InputWeek(employee.FullTime, weekNo);
         employee.LessHoursReason3 = InterfaceElemnts.LessHoursReason(weekNo, employee.week3, employee.FullTime);
         weekNo += 1;
-        
+
         employee.week4 = InterfaceElemnts.InputWeek(employee.FullTime, weekNo);
         employee.LessHoursReason4 = InterfaceElemnts.LessHoursReason(weekNo, employee.week4, employee.FullTime);
-        
+
         //call afunction to calaculate the late in panlety from class employee
         employee.Late = InterfaceElemnts.Late();
-        if (employee.Late){
+        if (employee.Late) {
             //input the number of late-in days by calling a method form IterfaceElemnts
             employee.LateDays = InterfaceElemnts.LateDaysInput();
             employee.LateDaysPen();
         }
-        
+
         //Get total working hours 
         employee.TotalWorkHours();
         employee.PriceMonth();
-        
+
         //Get Gross Pay 
         employee.GrossPay();
-        
+
         //get extra bonus and monthly reward
         employee.ExtraBonus();
         employee.MonthlyReward();
-        
+
         //Get Gross Pay after addition
         employee.GrossPayAfter();
-        
+
         //Calculate the deduction 
         employee.EPF();
         employee.SOCSO();
         employee.IncomeTax();
         employee.TotalDeduction();
-    
+
+        //NetPay
+        employee.NetPay();
+
         // Prepare to print either part or full time 
         String Time;
-        if (employee.FullTime == true)
+        if (employee.FullTime == true) {
             Time = "Full-Time";
-        else
+        } else {
             Time = "Part-Time";
+        }
         // Prepare week Paymanet to print 
         String totw1 = employee.PriceWeek(employee.week1, employee.LessHoursReason1);
         String totw2 = employee.PriceWeek(employee.week2, employee.LessHoursReason2);
@@ -220,7 +220,7 @@ public class MyFrontend {
         String totw4 = employee.PriceWeek(employee.week4, employee.LessHoursReason4);
         // create Id 
         employee.GenerateID(ID);
-        
+
         // print the payment slip  
         System.out.println(
                 "***********************UNIMY REMUNERATION SYSTEM***********************");
@@ -231,9 +231,9 @@ public class MyFrontend {
         System.out.println(
                 "**************************PAY SLIP - NOV 2021**************************");
         System.out.println(
-                "Name\t\t\t: "+ employee.Name);
+                "Name\t\t\t: " + employee.Name);
         System.out.printf(
-                "Staff ID\t\t: %s\n",employee.ID);
+                "Staff ID\t\t: %s\n", employee.ID);
         System.out.println(
                 "Status\t\t\t: " + Time);
         System.out.println(
@@ -242,7 +242,7 @@ public class MyFrontend {
                 "******************************* INCOME *******************************");
         System.out.printf("%-20s%-20s%-20s%-20s", "Working Hours", "Reason", "Per-Hour", "Total");
         System.out.println("");
-        System.out.format("Week 1 : %-11s%-20s%-20s%-20s", employee.week1, employee.LessHoursReason1, "RM" + employee.PriceHour, totw1 );
+        System.out.format("Week 1 : %-11s%-20s%-20s%-20s", employee.week1, employee.LessHoursReason1, "RM" + employee.PriceHour, totw1);
         System.out.println("");
         System.out.format("Week 2 : %-11s%-20s%-20s%-20s", employee.week2, employee.LessHoursReason2, "RM" + employee.PriceHour, totw2);
         System.out.println("");
@@ -254,32 +254,24 @@ public class MyFrontend {
         System.out.printf("\t\t\t\t\t\t    Total : RM%.2f\n", employee.MonthPrice);
         System.out.println(
                 "*********************** ALLOWANCE/REWARDS/BONUS ***********************");
-        System.out.printf("ALLOWANCE\t: RM%.2f\n", employee.Allowance);
+        System.out.printf("ALLOWANCE\t\t: RM%.2f\n", employee.Allowance);
         System.out.printf("EXTRA BONUS\t\t: RM%.2f\n", employee.ExtraBonus);
-        System.out.printf("REWARD("+employee.MonthlyRewardPercent+")"+"\t\t: RM%.2f\n", employee.MonthlyReward);
+        System.out.printf("REWARD(" + employee.MonthlyRewardPercent + ")" + "\t\t: RM%.2f\n", employee.MonthlyReward);
         System.out.printf("GROSS PAY\t\t: RM%.2f\n", employee.GrossPay1);
         System.out.println(
                 "****************************** PENALTIES ******************************");
-        System.out.printf("Late-in : "+employee.LateDays+" days\t\t\t Late-in Panelties RM%.2f\n", employee.LateDayPen);
+        System.out.printf("Late-in : " + employee.LateDays + " days\t\t\t Late-in Panelties RM%.2f\n", employee.LateDayPen);
         System.out.printf("New gross pay after penalties\t\t: RM%.2f\n", employee.GrossPayAfter);
         System.out.println(
                 "****************************** DEDUCTION ******************************");
         System.out.printf("EPF (11%%)\t\t: RM%.2f\n", employee.EPF);
         System.out.printf("SOCSO (0.5%%)\t\t: RM%.2f\n", employee.SOCSO);
-        System.out.printf("INCOME TAX(3%%)"+"\t\t: RM%.2f\n", employee.IncomeTax);
+        System.out.printf("INCOME TAX(3%%)" + "\t\t: RM%.2f\n", employee.IncomeTax);
         System.out.printf("TOTAL DEDUCTION\t\t: RM%.2f\n", employee.TotalDeduction);
         System.out.println(
-                "****************************** PENALTIES ******************************");
-        
-        
-    
-    
-    }   
-}   
-       
-     
-        
-        
-        
-        
-    
+                "******************************* NET PAY *******************************");
+        System.out.printf(
+                "NET PAY\t\t\t: RM%.2f\n", employee.NetPay);
+
+    }
+}
--- PartB/src/Myclasses/Employee.java
+++ PartB/src/Myclasses/Employee.java
@@ -1,109 +1,112 @@
-
 package Myclasses;
 
+public class Employee {
 
-public class Employee{
-    
-    public String Department; 
+    public String Department;
     public int StaffNO;
-    public String Name; 
-    public String ID; 
-    public String Position; 
-    public boolean FullTime; 
-    public double PriceHour ; 
+    public String Name;
+    public String ID;
+    public String Position;
+    public boolean FullTime;
+    public double PriceHour;
     public int week1;
     public int week2;
     public int week3;
     public int week4;
-    public int TotalWorkHours; 
-    public double MonthPrice; 
+    public int TotalWorkHours;
+    public double MonthPrice;
     public double GrossPay1;
-    public boolean Late; 
+    public boolean Late;
     public int LateDays;
     public double LateDayPen;
     public double GrossPayAfter;
     //for son class
-    public double Allowance;  
-    public String MonthlyRewardPercent; 
-    public double MonthlyReward ; 
+    public double Allowance;
+    public String MonthlyRewardPercent;
+    public double MonthlyReward;
     public String LessHoursReason1;
     public String LessHoursReason2;
     public String LessHoursReason3;
     public String LessHoursReason4;
     public double IncomeTax;
-    public double EPF; 
-    public double SOCSO; 
-    public double TotalDeduction; 
+    public double EPF;
+    public double SOCSO;
+    public double TotalDeduction;
     public double NetPay;
     //for son class
     public double ExtraBonus;
-    
-    public void SetPrice(){}
-    
-    public void GenerateID(int id){
-        this.ID = "UM"+this.Position.charAt(0)+"00"+id; 
+
+    public void SetPrice() {
     }
 
-     
-    public String PriceWeek(int weekhours, String LessHoursReason){
+    public void GenerateID(int id) {
+        this.ID = "UM" + this.Position.charAt(0) + String.format("%04d", id);
+    }
+
+    public String PriceWeek(int weekhours, String LessHoursReason) {
         String WeekPrice;
-        if ("Not Given".equals(LessHoursReason)){
+        if ("Not Given".equals(LessHoursReason)) {
             WeekPrice = "Incomplete";
-        }
-        else {
+        } else {
             double weekPay = this.PriceHour * weekhours;
-            WeekPrice = "RM"+weekPay;;
+            WeekPrice = "RM" + weekPay;
         }
-        return WeekPrice; 
+        return WeekPrice;
     }
-    
-    
-    public void TotalWorkHours(){
-        if (!"n".equals(this.LessHoursReason1)){
-            this.TotalWorkHours += this.week1; 
+
+    public void TotalWorkHours() {
+        if (!"n".equals(this.LessHoursReason1)) {
+            this.TotalWorkHours += this.week1;
         }
-        if (!"n".equals(this.LessHoursReason2)){
-            this.TotalWorkHours += this.week2; 
+        if (!"n".equals(this.LessHoursReason2)) {
+            this.TotalWorkHours += this.week2;
         }
-        if (!"n".equals(this.LessHoursReason3)){
-            this.TotalWorkHours += this.week3; 
+        if (!"n".equals(this.LessHoursReason3)) {
+            this.TotalWorkHours += this.week3;
         }
-        if (!"n".equals(this.LessHoursReason4)){
-            this.TotalWorkHours += this.week4; 
-        }
-    }
-    
-    
-    public void PriceMonth(){
-        this.MonthPrice = this.TotalWorkHours * this.PriceHour; 
-    }
-    
-    
-    public void LateDaysPen(){
-        if (this.Late){
-            this.LateDayPen = this.LateDays *  30 ;
+        if (!"n".equals(this.LessHoursReason4)) {
+            this.TotalWorkHours += this.week4;
         }
     }
-    
-    public void GrossPay(){
+
+    public void PriceMonth() {
+        this.MonthPrice = this.TotalWorkHours * this.PriceHour;
+    }
+
+    public void LateDaysPen() {
+        if (this.Late) {
+            this.LateDayPen = this.LateDays * 30;
+        }
+    }
+
+    public void GrossPay() {
         this.GrossPay1 = this.MonthPrice;
     }
-    
-    public void GrossPayAfter(){
-        this.GrossPayAfter = this.GrossPay1 - this.LateDayPen; 
+
+    public void GrossPayAfter() {
+        this.GrossPayAfter = this.GrossPay1 - this.LateDayPen;
     }
-    
-    public void MonthlyReward(){}
-    
-    public void EPF(){}
-    
-    public void SOCSO(){}
-    
-    public void IncomeTax(){}
-    
-    public void TotalDeduction(){}
-    
-    public void NetPay(){}
-    
-    public void ExtraBonus(){}
+
+    public void MonthlyReward() {
+    }
+
+    public void EPF() {
+    }
+
+    public void SOCSO() {
+    }
+
+    public void IncomeTax() {
+    }
+
+    public void TotalDeduction() {
+    }
+
+    public void NetPay() {
+        this.NetPay = this.GrossPayAfter - this.TotalDeduction;
+    }
+
+    public void ExtraBonus() {
+    }
+
 }
