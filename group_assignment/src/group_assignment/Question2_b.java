5650e6bdd3937827227223976000b166e29ffecf
theranae <ranaarafa28@gmail.com>
2021-12-28 12:17:53 +0300

    updated and finalized answers

--- group_assignment/src/group_assignment/Question2_b.java
+++ group_assignment/src/group_assignment/Question2_b.java
@@ -2,8 +2,7 @@
 /**
  * ARRAY COMPARISON CORRECTION
  * 
- * We have two ways to compare arrays either equals()
- * or deepEquals() methods from Arrays class.
+ * In this project we have different methods to compare arrays.
  * 
  * @author (RANA MAHAMOUD SAEED - B09200009)
  * @version 1.3
@@ -11,12 +10,16 @@
 
 package group_assignment;
 
-//importing array class from java.util package
+//importing array class from java.util package to get access to its methods
 import java.util.Arrays;
 
 public class Question2_b {
      public static void main (String[] args) 
  {
+     /**
+      * Method 1.
+      */
+     
  //creating, declaring and initialzing two arrays with same elements to compare
  int arr1[ ] = {34, 54, 90, 45, 89, 42, 88, 22, 19, 49};
  int arr2[ ] = {34, 54, 90, 45, 89, 42, 88, 22, 19, 49};
@@ -31,17 +34,27 @@ public class Question2_b {
  
  
  /**
-  * using if...else condition along with Arrays.equals method from Arrays class
-  * to compare the elements of the arrays.
+  * using if...else condition along with Arrays.equals method
+  * to compare the elements of the arrays and to print a certain statement
+  * if the condition applies or to print another
+  * statement if the condition doesn't apply
  */
  if (Arrays.equals(arr1,arr2))
+     //print statement if true
  System.out.println("Yes, They are same");
  else
+     //print statement if false
  System.out.println("No, They are not same");
  
  
  /**
-  * DEEP COMPARING.
+      * Method 2.
+      */
+ 
+ /**
+  * To deep compare, we first create an array of array that is of object
+  * reference type and then we an be able to use the deepEquals() 
+  * method from Arrays class.
   */
  
  //creating, declaring and initializing two string arrays to compare.
@@ -49,24 +62,157 @@ public class Question2_b {
  String arr4[ ] = {"Java", "Python", "C"};
  
   
- //this statement allow us to create an array of an array that is object reference type.
+ //this statement creates an array of an array that is object reference type.
  Object[] arr31 = {arr3};
  Object[] arr41 = {arr4};
  
+ //printing headline
  System.out.println("\n\tCOMPARING BETWEEN ARRAY 3 AND ARRAY 4\n");
  System.out.println("Array 3 = "+Arrays.toString(arr3));
  System.out.println("Array 4 = "+Arrays.toString(arr4));
  System.out.println("\nAre ARRAY 3 and ARRAY 4 the same?");
   
  /**
-  * using if...else along with Arrays.deepEquals method from Arrays class to 
-  * compare the arrays field by field recursively.
+  * using if...else statement along with Arrays.deepEquals method to 
+  * compare the arrays of object reference type; field by field recursively.
  */
  if (Arrays.deepEquals(arr31,arr41))
+     //statement to be printed if condition returns true
  System.out.println("Yes, They are same");
  else
- System.out.println("No, they are not same");
+      //statement to be printed if condition returns true
+ System.out.println("No, They are not same");
  
  
- }
-}
+ /**
+      * Method 3.
+      */
+ /**
+  * This method is called the naive approach and is used to compare arrays
+  * using if..else condition but for the else condition we will have a
+  * for loop and an if condition within the loop.
+  * 
+  */
+ 
+      //creating, declaring and initializing two integer arrays to compare.
+        int z[] = {12, 13, 14, 15};
+        int y[] = {12, 13, 14, 15};
+        
+    //printing headlines
+         System.out.println("\n\tCOMPARING BETWEEN ARRAY z AND ARRAY y\n");
+         System.out.println("Array z = "+Arrays.toString(z));
+         System.out.println("Array y = "+Arrays.toString(y));
+         System.out.println("\nAre ARRAY z and ARRAY y the same?");
+        
+         //using if...else condition
+         
+         //here, we have if statement where its condition is to check if 
+         //the size of array z and array y are not equal and to print a certain 
+         //output incase of the arrays being not equal.
+        if (z.length != y.length)
+  
+            System.out.println("No, They are not same");
+        
+        //else statement that has a for loop within
+        else {
+            //for loop where the second condition, i is to be less than the size of array z
+            for (int i = 0; i < z.length; i++)
+            {   
+                // if statement to compare each and every element in both arrays
+                if (z[i] != y[i])
+                {
+                    //printing statement
+                    System.out.println("No, They are not same");
+                    //exiting system at 0 to print 1 time only.
+                    System.exit(0);
+                }
+            }
+            //printing the following statement if the above conditions didn't match
+            System.out.println("Yes, They are same");
+        
+    }
+        /**
+      * Method 4.
+      */
+        
+        /**
+         * using boolean to compare arrays as well as .equals() method from Arrays class which will 
+         * compare the elements of first array to the second array.
+         * 
+         */
+      
+        //creating, declaring and initializing two string arrays to compare.
+      String[] flavour1 = {"Chocolate", "Strawberry", "Vanilla", "Toffee"};
+      String[] flavour2 = {"Chocolate", "Strawberry", "Vanilla", "Toffee"};
+
+      //printing headlines
+         System.out.println("\n\tCOMPARING BETWEEN ARRAY flavour1 AND ARRAY flavour2\n");
+         System.out.println("Array flavour1 = "+Arrays.toString(flavour1));
+         System.out.println("Array flavour2 = "+Arrays.toString(flavour2));
+    
+    //declaring result of boolean data type     
+   boolean result;
+   
+   //result will return either true or false for the comparing of the first and second arrays
+   result = Arrays.equals(flavour1, flavour2);
+ 
+   //printing statement with result
+   System.out.println("\nIs flavour 1 equal to flavour 2? \n" + result);
+ 
+   
+   /**
+      * Method 5.
+      */
+  /**
+   * Using boolean to compare arrays but in a
+   * different approach than the previous one mentioned.
+   * 
+   */
+  
+  //creating, declaring and initializing two integer arrays to compare.
+   char [] e ={'a', 'b','c','d'};
+   char [] f ={'a', 'b','c','d'};
+   
+   //printing headlines and arrays
+         System.out.println("\n\tCOMPARING BETWEEN ARRAY e AND ARRAY f\n");
+         System.out.println("Array e = "+Arrays.toString(e));
+         System.out.println("Array f = "+Arrays.toString(f));
+         System.out.println("\nAre ARRAY e and ARRAY f the same?");
+         
+   //using if statement where its condition is if the result is true to print a certain statement       
+   if(result(e,f)){
+       System.out.println("Yes, They are same");
+   }
+   //else statement to print a certain statement if the result return false
+   else{
+       System.out.println("No, They are not same");
+   }
+   }
+     //here, we created a method namely result that is a boolean data type
+     //with two parameters that are arrays e and f, 
+     //this method returns true if both arrays are equal
+     //and returns false otherwise.
+     public static boolean result(char []e, char []f){
+         
+        //if statement where its condition is to check if the sizes of both arrays are equal 
+        // or not and to return false if they are not equal
+         if (e.length!=f.length){
+             return false;
+         }
+         //for loop where its second condition, x is to be less than the size of array f
+             for (int x=0; x<f.length;x++){
+                 //if statement to compare between the elemnts of both arrays an to return false if 
+                 //the elements are not equal
+                 if (e[x] != f[x]){
+                     return false;
+                 }
+             }
+             //this returns true if the above conditions did not match.
+             return true;
+     }
+    }
+     
+      
+    
+
+ 
--- group_assignment/src/group_assignment/Question2_c.java
+++ group_assignment/src/group_assignment/Question2_c.java
@@ -16,36 +16,47 @@ public class Question2_c {
         
         System.out.println("\tARRAY COPYING (with applied changes to the original array)\n");
         
-        
+         /**
+      * Method 1.
+      */
+         
         /**
-         * Using variable Assignment to copy array...
+         * Using variable Assignment to copy array.
          * 
          */
         
         /**
-         * this method has its side effects because any change that happens 
-         * to array a can affect array b
+         * This method has its side effects because any change that happens 
+         * to array a can affect array b.  
          * 
          */
         
         //Creating, declaring and initializing array a 
         int a[]= {32, 33, 34, 35, 36,40};
-        //Creating, declaring array b and initializing it to array a
+        
+        //Creating and declaring array b, and assigning it to array a. 
+        //this assignment is wrong because array b will be assigned to array a as a reference
+        //therefore, any changes that happen to array a affects array b and vice versa.
         int [] b = a;
        
-        //changing the element in index 2 for array a to test the changes in array b
+        //changing the element in index 0 for array a to test changes in array b
         a[0] = 0;
         
-        //printing array a using toString method from Arrays class
+        //printing array a using .toString method from Arrays class
         System.out.println("Array a = "+Arrays.toString(a));
 
         //printing array b using toString method from Arrays class
         System.out.println("Array b = "+Arrays.toString(b));
         
         
+        /**
+      * Method 2.
+      */
         
         /**
-         * Using iterating to copy array....
+         * Using for loop to iterate and copy the elements of 
+         * the first array to the second array.
+         * 
         */
         
         //Creating, declaring and initializing array c
@@ -57,18 +68,23 @@ public class Question2_c {
         //changing element of array c to test changes in array d
         
         /**
+         * ******OBSERVATION*******
          * if we applied changes in this stage we see that the output 
-         * of array d will be changed too. to be cont.
+         * of the copied array will be changed too.
+         * to be continued.
          */
          c[0] = 0;
         
-        //using for loop to iterate and copy each element
+        //using for loop,where its second condition, i is to be less than the size of array d,
+        //to be able to iterate and copy each element from array c to d
         for (int i = 0; i<d.length; i++){
+            
+            //elements of array d and array c are equal.
             d[i]=c[i];
         }
         
         /**
-         * cont.. if we applied changes to array c in this stage, then array d 
+         * continuation.. if we applied changes to array c in this stage, then array d 
          * won't go through changes, it will maintain the first elements
          * of array c.
          */
@@ -79,17 +95,23 @@ public class Question2_c {
         //printing array d using toSting method from Arrays class
         System.out.println("Array d  = "+Arrays.toString(d));
         
-    
+     /**
+      * Method 3.
+      */
+     
     
     /**
-     * Using clone to copy array...
+     * Using clone method to copy array is the most basic and effective way 
+     * to copy any arrays.
      * 
      */
     
     //Creating, declaring and initializing array e
     int [] e = {55, 54, 56, 58, 59, 60};
     
-    //creating and declaring array f and initializing it to array e using .clone()
+    //creating and declaring array f and initializing it to array e using .clone() and this 
+    //method is to copy the elements of the original array to the copied array
+    //instead of referring to it. therefore any changes happen to any array won't affect the other.
     int []f = e.clone();
     
     //changing element of array e to test changes in array f
@@ -102,20 +124,29 @@ public class Question2_c {
     System.out.println("Array f = "+Arrays.toString(f));
    
     
-    
+     /**
+      * Method 4.
+      */
+     
     /**
-     * Using arraycopy...
+     * Using arraycopy() method from System class to copy arrays.
      * 
      */
     
     //Creating, declaring and initializing array e
     int[] g = {62, 64, 66, 68, 69, 70};
     
-    //Creating and declaring array d and initializing it to the length of array g
+    //Creating and declaring array d and initializing it's length to the length of array g
     int [] h = new int [g.length];
     
-    //using System.arraycopy to copy elements of array g to array h
-    System.arraycopy(g, 0, h, 0, h.length);
+    /**using System.arraycopy to copy elements of array g to array h
+    *this method has 5 parameters where the first parameter is the original array
+    *and the second parameter is the number of index to start copying from
+    *the third parameter is the copied array
+    *the fourth parameter is the number of index to store the copied elements
+    *the fifth parameter is the length of the number of elements to be copied.
+    */
+    System.arraycopy(g, 0, h, 0, g.length);
     
     //applying changes array g to test array h
     g[0] = 0;
@@ -126,9 +157,12 @@ public class Question2_c {
     //print array h using toString method from Arrays class
     System.out.println("Array h = "+Arrays.toString(h));
     
-    
+     /**
+      * Method 5.
+      */
+     
     /**
-     * Using copyof() to copy arrays...
+     * Using copyof() method from Arrays class to copy arrays.
      * 
      */
     
@@ -136,10 +170,12 @@ public class Question2_c {
     int [] j = {22, 24, 26, 28, 29, 30};
     
     //Creating and declaring array k and using Arrays.copyof to copy the elements
-    //of array j to array k
+    //of array j to array k.
+    //This method has two parameters, the first parameter is the original array 
+    //and the second parameter is the number of elements to be copied.
     int k[] = Arrays.copyOf(j,6 );
     
-    //applying changes array j to test array k
+    //applying changes in array j to test if array k will be affected
     j[0] = 0;
     
     //print array h using toString method from Arrays class
@@ -147,38 +183,42 @@ public class Question2_c {
     
     System.out.print("Array k = [ ");
     
-    //using for loop to iterate and print the elements of array k
-    for (int i = 0; i < k.length; i++){
-        
-            System.out.print(k[i] + " ");
+    //using enhanced for loop to print the elements of array k.
+    for(int num : k){
+        System.out.print(num + " ");
     }
-            System.out.println("] ");
-            
-            
-            
+    System.out.print("]");
+   
+      /**
+      * Method 6.
+      */   
+      
     /**
-     * Using Arrays.copyOfRange() to copy arrays...
+     * Using .copyOfRange() method from Arrays class to copy arrays.
      * 
      */
     
      //Creating, declaring and initializing array j
     int [] m = {1, 3, 5, 7, 9, 10};
     
-    //Creating and declaring array k and using Arrays.copyOfRange to copy
+    //Creating and declaring array n and using .copyOfRange() method to copy
     //the elements of array m to array n.
+    //this method has three parameters, first one being the original array
+    //the second being the number of indexes to copy from
+    //and the third parameter is the number of elements to copy.
     int [] n = Arrays.copyOfRange(m, 0, 6);
     
     //applying changes to array m to observe changes in array n
     m[0] = 0;
     
     //print array h using toString method from Arrays class
-    System.out.println("\nArray m = "+Arrays.toString(m));
+    System.out.println("\n\nArray m = "+Arrays.toString(m));
     
-    System.out.print("Array n from m = [");
+    System.out.print("Array n = [");
     
-    //using for loop to iterate and print the elements of array n
-    for (int i = 0; i < n.length; i++){
-             System.out.print(n[i] + " ");
+    //using enhanced for loop to print the copied array
+    for(int numbers : n){
+        System.out.print(numbers + " ");
     }
               System.out.println("]");
     }
